c79b3d22f1042021709d3001582571387b2a8294
Kwongster <bryant.j.kwong@gmail.com>
2024-02-24 22:38:50 -0800

    Add ChatOpenAI model and process input using ChatPromptTemplate

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import os
-
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
 
 load_dotenv()
 
+OPENAI_MODEL = "gpt-3.5-turbo"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+output_parser = StrOutputParser()
+
+
+
 
 def main():
-    print(OPENAI_API_KEY)
-    pass
+   prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are world class technical documentation writer."),
+    ("user", "{input}")
+])
+   llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+   chain = prompt | llm | output_parser
+   print(chain.invoke({"input": "how can langsmith help with testing?"}))
+
+
+
+
+    
+
 if __name__ == "__main__":
     main()
