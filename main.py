58307453d9969ea61701fc31136a762bbc97fea7
Kwongster <bryant.j.kwong@gmail.com>
2024-03-04 18:46:35 -0800

    install octoai

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["OCTOAI_API_TOKEN"] = "OCTOAI_API_TOKEN"
+os.environ["OCTOAI_API_TOKEN"] = "YOUR_OCTOAI_API_TOKEN"
 os.environ["ENDPOINT_URL"] = "https://text.octoai.run/v1/chat/completions"
 
 from langchain.chains import LLMChain
@@ -9,3 +9,26 @@ from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
 
 template = """Below is an instruction that describes a task. Write a response that appropriately completes the request.\n Instruction:\n{question}\n Response: """
 prompt = PromptTemplate.from_template(template)
+
+llm = OctoAIEndpoint(
+    model_kwargs={
+        "model": "gemma-7b-it",
+        "max_tokens": 1024,
+        "presence_penalty": 0,
+        "temperature": 0.1,
+        "top_p": 0.9,
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant. Keep your responses limited to one short paragraph if possible.",
+            },
+        ],
+    },
+)
+
+question = "Who was leonardo davinci?"
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+print(llm_chain.invoke(question))
+print("hello")
