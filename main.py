f506fd7f37980b984b9073bbc3f77252b7832c24
Kwongster <bryant.j.kwong@gmail.com>
2024-03-04 18:33:34 -0800

    Update API token and endpoint URL, refactor code structure.-

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
 import os
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-load_dotenv()
+os.environ["OCTOAI_API_TOKEN"] = "OCTOAI_API_TOKEN"
+os.environ["ENDPOINT_URL"] = "https://text.octoai.run/v1/chat/completions"
 
-OPENAI_MODEL = "gpt-3.5-turbo"
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-output_parser = StrOutputParser()
-loader = WebBaseLoader("https://docs.smith.langchain.com")
-docs = loader.load()
-embeddings = OpenAIEmbeddings()
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(docs)
-vector = FAISS.from_documents(documents, embeddings)
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
 
-
-
-def main():
-   prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
-   llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
-   chain = prompt | llm | output_parser
-   print(chain.invoke({"input": "how can langsmith help with testing?"}))
-
-
-
-
-    
-
-if __name__ == "__main__":
-    main()
+template = """Below is an instruction that describes a task. Write a response that appropriately completes the request.\n Instruction:\n{question}\n Response: """
+prompt = PromptTemplate.from_template(template)
