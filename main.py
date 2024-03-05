7d075af2e61b522c26200b6b555da76ea76d1a77
Kwongster <bryant.j.kwong@gmail.com>
2024-03-04 20:59:49 -0800

    Refactor JSON format and user preferences handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -16,18 +17,32 @@ userLiquor = "vodka"
 userFlavor = "sweet"
 userMood = "celebratory"
 instructions = "create a unique creative advance cocktail based on the user preferences in the text delimited by triple periods"
-json_format = """{"name": "Sour Nostalgia", "description": "A unique cocktail with a nostalgic twist, featuring a sour flavor profile with a hint of nostalgia", "ingredients": [{"name": "Vodka", "quantity": "2 oz"}, {"name": "Lemon Juice", "quantity": "1 oz"}], "instructions": "Add all ingredients to a cocktail shaker without ice. Dry shake vigorously for 10-15 seconds. Add ice and shake again until well chilled"}"""
-userPreferences = "contains {userLiquor} and emphasizes a {userFlavor} flavor profile for a {userMood} mood"
-negative = "Do not include ${userFlavor}, ${userLiquor}, or ${userMood} in the recipe name."
+jsonformat = {
+    "name": "Sour Nostalgia",
+    "description":
+      "A unique cocktail with a nostalgic twist, featuring a sour flavor profile with a hint of nostalgia",
+    "ingredients": [
+      {
+        "name": "Vodka",
+        "quantity": "2 oz",
+      },
+      {
+        "name": "Lemon Juice",
+        "quantity": "1 oz",
+      },
+    ],
+    "instructions":
+      "Add all ingredients to a cocktail shaker without ice. Dry shake vigorously for 10-15 seconds. Add ice and shake again until well chilled",
+  };
+
+userPreferences = f"{userLiquor} and emphasizes a {userFlavor} flavor profile for a {userMood} mood"
+negative = f"Do not include {userFlavor}, {userLiquor}, or {userMood} in the recipe name."
 template = instructions + negative + json_format + f"...{userPreferences}..."
 prompt = PromptTemplate.from_template(template)
 
-
-
-
 llm = OctoAIEndpoint(
     endpoint_url=ENDPOINT_URL,
-    octoai_api_token=octoai_api_token,  # Pass token as a named parameter
+    octoai_api_token=octoai_api_token, 
     model_kwargs={
         "model": "gemma-7b-it",
         "max_tokens": 1024,
@@ -43,7 +58,7 @@ llm = OctoAIEndpoint(
     },
 )
 
-inputs = {"userLiquor": userLiquor, "userFlavor": userFlavor,"userMood": userMood}  
+inputs = {"name": "Sour Nostalgia", "userLiquor": userLiquor, "userFlavor": userFlavor,"userMood": userMood}
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-print(llm_chain.invoke(inputs)["text"])
+print(llm_chain.invoke(inputs))
