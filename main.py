fef876c443917bedd43f42e01cdeb022c3b0640d
Kwongster <bryant.j.kwong@gmail.com>
2024-03-04 20:40:25 -0800

    Update main.py with new cocktail recipe prompt template

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import os
 from dotenv import load_dotenv
-
-load_dotenv()  # Load API credentials from .env file
-
-octoai_api_token = os.getenv("OCTOAI_API_TOKEN")  # Retrieve token
-
-if not octoai_api_token:  # Check if token is retrieved successfully
-    raise ValueError("OCTOAI_API_TOKEN not found in .env file or environment variables.")
-
-ENDPOINT_URL = "https://text.octoai.run/v1/chat/completions"  # Set endpoint URL
-
-
-
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
 
-json_format = """{"name": "Sour Nostalgia", "description": "A unique cocktail with a nostalgic twist, featuring a sour flavor profile with a hint of nostalgia", "ingredients": [{"name": "Vodka", "quantity": "2 oz"}, {"name": "Lemon Juice", "quantity": "1 oz"}], "instructions": "Add all ingredients to a cocktail shaker without ice. Dry shake vigorously for 10-15 seconds. Add ice and shake again until well chilled"}"""
-template = """Complete the following steps: \n 1. Create a drink recipe contain {liquor} and is sweet \n 2. Output the response as json with the following fields:{json_format} """
+load_dotenv() 
 
+octoai_api_token = os.getenv("OCTOAI_API_TOKEN") 
+if not octoai_api_token:  # Check if token is retrieved successfully
+    raise ValueError("OCTOAI_API_TOKEN not found in .env file or environment variables.")
+
+ENDPOINT_URL = "https://text.octoai.run/v1/chat/completions"
+
+template = """Complete the following steps: \n 1. Create a drink recipe contain {liquor} and is sweet \n 2. Output the response as json with the following fields:{json_format} """
 prompt = PromptTemplate.from_template(template)
+liquor = "Soju"
+json_format = """{"name": "Sour Nostalgia", "description": "A unique cocktail with a nostalgic twist, featuring a sour flavor profile with a hint of nostalgia", "ingredients": [{"name": "Vodka", "quantity": "2 oz"}, {"name": "Lemon Juice", "quantity": "1 oz"}], "instructions": "Add all ingredients to a cocktail shaker without ice. Dry shake vigorously for 10-15 seconds. Add ice and shake again until well chilled"}"""
 
 llm = OctoAIEndpoint(
     endpoint_url=ENDPOINT_URL,
@@ -39,7 +35,7 @@ llm = OctoAIEndpoint(
     },
 )
 
-liquor = "Soju"
+
 inputs = {"liquor": liquor, "json_format": json_format}  
 
 llm_chain = LLMChain(prompt=prompt, llm=llm)
