a37cf52addb22b4cafb3296c4f1e0fa3bb6e3504
Kwongster <bryant.j.kwong@gmail.com>
2024-03-05 11:17:33 -0800

    Add Recipe class and update PromptTemplate

--- tutorial/json_parse.py
+++ tutorial/json_parse.py
@@ -1,6 +1,7 @@
 
 import os
 from dotenv import load_dotenv
+from typing import List
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
@@ -11,9 +12,20 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
 
-prompt = PromptTemplate.from_template("Create a food recipe based on {cuisine} cuisine")
-model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-3.5-turbo-0125")
-output_parser = JsonOutputParser()
+class Recipe(BaseModel):
+    name: str = Field(description="Name of the recipe")
+    ingredients: List[str] = Field(description="List of ingredients")
+    instructions: List[str] = Field(description="List of cooking instructions")
+    # servings: Optional[int] = Field(description="Number of servings")
+    # cook_time: Optional[str] = Field(description="Total cooking time")
 
-chain = prompt | model |output_parser
+parser = JsonOutputParser(pydantic_object=Recipe)
+prompt = PromptTemplate(
+    template="Create a food recipe based on {cuisine} cuisine",
+    input_variables=["cuisine"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-3.5-turbo-0125")
+
+chain = prompt | model | parser
 print(chain.invoke({"cuisine": "japanese"}))
