9c34ec89039f1f6945701da582e8bd421dff57be
Kwongster <bryant.j.kwong@gmail.com>
2024-03-06 10:20:09 -0800

    Add cocktail recipe generation function and endpoint

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-# activate virtual env - go to .venvscripts
-# enter: .\Activate.ps1
+# pip install python-dotenv langchain "fastapi[all]" octoai-sdk 
+# .venv\Scripts\Activate.ps1
+# uvicorn main:app --reload
 
-from langcorn import create_service
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-app = create_service(
-    "cocktail:chain "
-)
+from cocktail import generate_cocktail_recipe
+user_liquor = "Whiskey"
+user_flavor = "Spicy"
+user_mood = "Relaxed"
+
+@app.get("/cocktail")
+async def test():
+    return generate_cocktail_recipe(user_liquor, user_flavor, user_mood)
