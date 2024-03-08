ba8ae737f8eb759d0a775348a53f88d357e4fd57
Kwongster <bryant.j.kwong@gmail.com>
2024-03-07 20:29:56 -0800

    Update CORS settings for production environment

--- backend/api.py
+++ backend/api.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
 
 app = FastAPI()
 
-origins = ["http://localhost:3000"]
+# if os.getenv("PRODUCTION_DOMAIN"):
+#     origins = [os.getenv("PRODUCTION_DOMAIN")]
+# else:
+#     origins = ["http://localhost:3000"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/")
 async def root():
