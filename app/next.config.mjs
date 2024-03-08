1f5d957f6dc8f720635cb72b168ca1ab164b5d08
Kwongster <bryant.j.kwong@gmail.com>
2024-03-07 18:13:30 -0800

    Update API route headers configuration

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   rewrites: async () => [
     {
-      source: "/", // Matches requests to /api/cocktail
+      source: "/cocktail", // Matches requests to /api/cocktail
       destination: "http://127.0.0.1:8000/cocktail", // Forward to backend URL
     },
     {
@@ -10,27 +10,27 @@ const nextConfig = {
       destination: "https://langchain-backend-alpha.vercel.app/cocktail", // Forward to backend URL
     },
   ],
-  //   async headers() {
-  //     return [
-  //       {
-  //         // matching all API routes
-  //         source: "/*",
-  //         headers: [
-  //           { key: "Access-Control-Allow-Credentials", value: "true" },
-  //           { key: "Access-Control-Allow-Origin", value: "*" },
-  //           {
-  //             key: "Access-Control-Allow-Methods",
-  //             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //           },
-  //           {
-  //             key: "Access-Control-Allow-Headers",
-  //             value:
-  //               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //           },
-  //         ],
-  //       },
-  //     ];
-  //   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
