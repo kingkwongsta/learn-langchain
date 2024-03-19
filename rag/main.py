96df582e0206a7f9aed6e654c39ebd9feb3b8e44
bkwong <bryant.j.kwong@gmail.com>
2024-03-18 21:23:49 -0700

    add book to chunk

--- rag/main.py
+++ rag/main.py
@@ -1,3 +1,12 @@
 # .venv\Scripts\Activate.ps1
+# cd .venv
 # source bin/activate
 # uvicorn main:app --reload
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+loader = UnstructuredPDFLoader("mlbook.pdf")
+data = loader.load()
+
+print (f'You have {len(data)} document(s) in your data')
+print (f'There are {len(data[0].page_content)} characters in your document')
