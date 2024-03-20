6608c23a26257aa3fa3495f82156ee1c62dd3914
bkwong <bryant.j.kwong@gmail.com>
2024-03-19 19:45:39 -0700

    switch to pyPDF loader

--- rag/main.py
+++ rag/main.py
@@ -2,11 +2,22 @@
 # cd .venv
 # source bin/activate
 # uvicorn main:app --reload
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# pip install pdfminer.six
+# pip install pdf2image
+# pip install opencv-python
+# pip install unstructured_inference
 
-loader = UnstructuredPDFLoader("mlbook.pdf")
+
+from langchain_community.document_loaders import UnstructuredPDFLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+loader = PyPDFLoader("mlbook.pdf")
 data = loader.load()
 
 print (f'You have {len(data)} document(s) in your data')
-print (f'There are {len(data[0].page_content)} characters in your document')
+print (f'There are {len(data[0].page_content)} characters in your sample document')
+print (f'Here is a sample: {data[0].page_content[:200]}')
