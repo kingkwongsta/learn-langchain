f9546addf2dd8747c6c4bb96f399ade1bc82a8a4
bkwong <bryant.j.kwong@gmail.com>
2024-04-22 14:50:54 -0700

    data set isntall and function to chunk parquet file

--- rag/loadvector.py
+++ rag/loadvector.py
@@ -4,51 +4,47 @@
 # uvicorn main:app --reload
 
 
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 import os
+
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
 
 load_dotenv()
 
 
-# from langchain_openai import OpenAIEmbeddings
-# from pinecone import Pinecone, ServerlessSpec
-# from langchain_pinecone import PineconeVectorStore
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 
+pc = Pinecone(api_key=PINECONE_API_KEY)
 
+use_serverless = True
 
+index_name = input("Which index do you want to load the data to? ")
 
-# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-# PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-
-# pc = Pinecone(api_key=PINECONE_API_KEY)
-
-# use_serverless = True
-
-# index_name = input("Which index do you want to load the data to? ")
-
-# spec = ServerlessSpec(cloud='aws', region='us-west-2')
+spec = ServerlessSpec(cloud='aws', region='us-west-2')
 
     
-# index = pc.Index(index_name)
+index = pc.Index(index_name)
 
 
 
-# model_name = 'text-embedding-ada-002'
+model_name = 'text-embedding-ada-002'
 
-# embeddings = OpenAIEmbeddings(
-#     model=model_name,
-#     openai_api_key=OPENAI_API_KEY
-# )
+embeddings = OpenAIEmbeddings(
+    model=model_name,
+    openai_api_key=OPENAI_API_KEY
+)
 
-# text_field = "text"
-# vectorstore = PineconeVectorStore(
-#     index, embeddings, text_field
-# )
+text_field = "text"
+vectorstore = PineconeVectorStore(
+    index, embeddings, text_field
+)
 
-#----- add data to vector store
-# docsearch = PineconeVectorStore.from_documents(texts, embeddings, index_name=index_name)
+# ----- add data to vector store
+docsearch = PineconeVectorStore.from_documents(texts, embeddings, index_name=index_name)
 
-# print(f"***** Successfully added data to Vector Store in index: {index_name} *****")
-# print(index.describe_index_stats())
+print(f"***** Successfully added data to Vector Store in index: {index_name} *****")
+print(index.describe_index_stats())
