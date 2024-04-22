27bb31e6616a9ca169d4bc9bd69853fd6c61de00
bkwong <bryant.j.kwong@gmail.com>
2024-04-22 14:40:51 -0700

    fix text splitter function parameters and provide some text content for logging

--- rag/chunkPDF.py
+++ rag/chunkPDF.py
@@ -6,14 +6,14 @@ def chunk_pdf(pdf, chunk_size, chunk_overlap):
     loader = PyPDFLoader(pdf)
     data = loader.load()
 
-    #chunk_size=500 | chunk_overlap=50
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size, chunk_overlap)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     texts = text_splitter.split_documents(data)
-    
-    print("***** PDF Successfully Chunk *****")
+    print("**************************************************")
+    print("<<<<<<<<<< PDF Successfully Chunk >>>>>>>>>>")
     print (f'You have {len(data)} document(s) in your data')
     print (f'There are {len(data[0].page_content)} characters in your sample document')
-    print (f'Here is a sample: {data[2].page_content[:200]}')
-
+    # print (f'Here is a sample: {data[80].page_content[:200]}')
+    print (f'Here is a sample: {data[80]}')
+    print("**************************************************")
     
     return texts
