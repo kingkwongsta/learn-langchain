246bdd9f7be430819f2b9fb3c8cf12a51f6b449e
bkwong <bryant.j.kwong@gmail.com>
2024-04-22 14:27:17 -0700

    split pdf chunker into individual function

---
+++ rag/chunkPDF.py
@@ -0,0 +1,19 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def chunk_pdf(pdf, chunk_size, chunk_overlap):
+
+    loader = PyPDFLoader(pdf)
+    data = loader.load()
+
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size, chunk_overlap)
+    texts = text_splitter.split_documents(data)
+    
+    print("***** PDF Successfully Chunk *****")
+    print (f'You have {len(data)} document(s) in your data')
+    print (f'There are {len(data[0].page_content)} characters in your sample document')
+    print (f'Here is a sample: {data[2].page_content[:200]}')
+
+    
+    return texts
--- rag/loadvector.py
+++ rag/loadvector.py
@@ -11,56 +11,44 @@ import os
 
 load_dotenv()
 
-loader = PyPDFLoader("./mlbook2.pdf")
-data = loader.load()
 
-# print (f'You have {len(data)} document(s) in your data')
-# print (f'There are {len(data[0].page_content)} characters in your sample document')
-# print (f'Here is a sample: {data[2].page_content[:200]}')
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-texts = text_splitter.split_documents(data)
-
-
-
-# from langchain_community.vectorstores import Pinecone
-from langchain_openai import OpenAIEmbeddings
-from pinecone import Pinecone, ServerlessSpec
-from langchain_pinecone import PineconeVectorStore
+# from langchain_openai import OpenAIEmbeddings
+# from pinecone import Pinecone, ServerlessSpec
+# from langchain_pinecone import PineconeVectorStore
 
 
 
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+# PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
+# pc = Pinecone(api_key=PINECONE_API_KEY)
 
-use_serverless = True
+# use_serverless = True
 
-index_name = input("Which index do you want to load the data to? ")
+# index_name = input("Which index do you want to load the data to? ")
 
-spec = ServerlessSpec(cloud='aws', region='us-west-2')
+# spec = ServerlessSpec(cloud='aws', region='us-west-2')
 
     
-index = pc.Index(index_name)
+# index = pc.Index(index_name)
 
 
 
-model_name = 'text-embedding-ada-002'
+# model_name = 'text-embedding-ada-002'
 
-embeddings = OpenAIEmbeddings(
-    model=model_name,
-    openai_api_key=OPENAI_API_KEY
-)
+# embeddings = OpenAIEmbeddings(
+#     model=model_name,
+#     openai_api_key=OPENAI_API_KEY
+# )
 
-text_field = "text"
-vectorstore = PineconeVectorStore(
-    index, embeddings, text_field
-)
+# text_field = "text"
+# vectorstore = PineconeVectorStore(
+#     index, embeddings, text_field
+# )
 
 #----- add data to vector store
-docsearch = PineconeVectorStore.from_documents(texts, embeddings, index_name=index_name)
+# docsearch = PineconeVectorStore.from_documents(texts, embeddings, index_name=index_name)
 
-print(f"***** Successfully added data to Vector Store in index: {index_name} *****")
-print(index.describe_index_stats())
+# print(f"***** Successfully added data to Vector Store in index: {index_name} *****")
+# print(index.describe_index_stats())
